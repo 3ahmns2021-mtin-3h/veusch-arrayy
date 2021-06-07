c97e488544549f43e23154ab4c65bb13db16db2a
veusch <veusch>
2021-06-07 09:25:48 +0200

    Inclass commit

--- Assets/MyGame/Scripts/PracticeArrays.cs
+++ Assets/MyGame/Scripts/PracticeArrays.cs
@@ -21,11 +21,50 @@ public class PracticeArrays : MonoBehaviour
 
         GameObject[] arrayColoredImagesRed = GameObject.FindGameObjectsWithTag("RedImage");
 
+        foreach(GameObject elem in arrayColoredImagesRed)
+        {
+            Debug.Log(elem.name);
+        }
+
+        Debug.Log("Element 0 von array 1 hat den Wert: " + array1[0]);
+        Debug.Log("Element 0 von array 2 hat den Wert: " + array2[0]);
+        Debug.Log("Element 0 von array 3 hat den Wert: " + array3[0]);
+        Debug.Log("Element 0 von array 4 hat den Wert: " + array4[0]);
+        Debug.Log("Name des ersten GameObjects " + arrayColoredImagesRed[0].name);
+
+
+        Debug.Log("Element 0 von array 1 hat den Wert: " + array1[array1.Length - 1]);
+        Debug.Log("Element 0 von array 2 hat den Wert: " + array2[array2.Length - 1]);
+        Debug.Log("Element 0 von array 3 hat den Wert: " + array3[array3.Length - 1]);
+        Debug.Log("Element 0 von array 4 hat den Wert: " + array4[array4.Length - 1]);
+        Debug.Log("Name des letzten     GameObjects " + arrayColoredImagesRed[arrayColoredImagesRed.Length - 1].name);
+        Debug.Log("Letztes Element von array 3 hat den Wert: " + array3[0] + array3[array3.Length - 1] + array4[0] + array4[array4.Length - 1]);
+        Debug.Log("Name des ersten GameObjects: " + arrayColoredImagesRed[arrayColoredImagesRed.Length - 1].name);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void PrintElements(int[] x)
+    {
+        string tmp = "";
+        for (int i = 0; i < x.Length; i++)
+        {
+            Debug.Log("Element Nummer: " + i + "hat den Wert: " + x[i]);
+        }
+
+    }
+
+    public void PrintElements(string[] x)
+    {
+        for (int i = 0; i < x.Length; i++)
+        {
+            Debug.Log("Element Nummer: " + i + "hat den Wert: " + x[i]);
+
+            Debug.Log(tmp);
+        }
+
     }
 }
